49357831e69c16d3a199532293edb8c2dacf4fb3
mateshamatav <kunichuk77@mail.ru>
2025-10-02 18:19:02 +0300

    first commit

---
+++ day0/classwork/lesson0/lesson0/lesson0.py
@@ -0,0 +1,18 @@
+car = "bmw"
+print(car)
+
+
+
+
+
+mom = "ana"
+dad = "bachuki"
+sister = "mari"
+
+print(mom)
+print(dad)
+print(sister)
+
+name = "mate"
+age = "13"
+print(name, age)
---
+++ day0/homework/lesson0/lesson0/lesson0.py
@@ -0,0 +1,57 @@
+# print(".")
+# print("..")
+# print("...")
+# print("....") 
+# print(".....")
+
+
+eat = "ხაჭაპური"
+eat1 = "ხინკალი"
+eat2 = "მწვადი"
+eat3 = "შაურმა"
+eat4 = "ბურგერი"
+
+
+
+print(eat)
+print(eat1)
+print(eat2)
+print(eat3)
+print(eat4)
+
+
+
+Hello = "mate"
+print(Hello)
+
+print("__________________")
+
+
+print("_  my name mate _")
+
+print("__________________")
+
+
+hamburgeri = "15 lariani 25, 30, 40, 50,"
+pizza = "30 lariani , 35 , 50 , 60, 70"
+cezari = "15 lariani 20, 25"
+fri = "8 lariani 14 ,18"
+shaurma = "10 lariani 15 ,20 ,25, 30"
+yvelisouzi = "8 lariani 13, 16,"
+kokakola = "3 laeiani 5"
+wyali = "1 lariani"
+cupiatmis = "3"
+cupiportoxlis = "3"
+
+
+
+print(hamburgeri)
+print(pizza)
+print(cezari)
+print(fri)
+print(shaurma)
+print(yvelisouzi)
+print(kokakola)
+print(wyali)
+print(cupiatmis)
+print(cupiportoxlis)
